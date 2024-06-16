bd0da6232ec37b88f43c5ee6ae84259e5e5c0b4c
Praditia Nur <pnplusplus@gmail.com>
2024-06-16 08:21:00 +0000

    Settings: Adapt device info entries to Kusuma versioning * Inspired by: - BootleggersROM/packages_apps_Settings@1483aba - crdroidandroid/android_packages_apps_Settings@998e978
    Co-authored-by: Hernán <eldainosor@users.noreply.github.com>
    Co-authored-by: Ganesh Varma <ganeshvarma@arrowos.net>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ib6a775bf9fd7253682d020081b6a973a9839e71d

---
+++ src/com/android/settings/deviceinfo/firmwareversion/ExtraVersionPreferenceController.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2024 Kusuma
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.os.SystemProperties;
+
+import com.android.settings.core.BasePreferenceController;
+
+public class ExtraVersionPreferenceController extends BasePreferenceController {
+
+    private static final String TAG = "ExtraVersionPreferenceController";
+    private static final String EXTRA_PRODUCT_VERSION_CODENAME = "ro.extra.version.codename";
+    private static final String EXTRA_PRODUCT_VERSION_NUMBER = "ro.extra.version.number";
+
+    public ExtraVersionPreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        String kusumaExtraVersionCodename = SystemProperties.get(EXTRA_PRODUCT_VERSION_CODENAME);
+        String kusumaExtraVersionNumber = SystemProperties.get(EXTRA_PRODUCT_VERSION_NUMBER);
+        return kusumaExtraVersionCodename + " (" +  kusumaExtraVersionNumber + ")";
+    }
+}
