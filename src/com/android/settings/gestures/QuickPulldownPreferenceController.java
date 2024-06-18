71bad341726c907e4370f9b756f9e4ca4c127b83
Praditia Nur <pnplusplus@gmail.com>
2024-06-18 02:01:23 +0000

    Settings: Import Quick Pulldown settings from LineageParts * And make it a settings page
    Change-Id: I062d1205583e0cf99be165dd2f1500c53cb79218

---
+++ src/com/android/settings/gestures/QuickPulldownPreferenceController.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2024 Kusuma
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.gestures;
+
+import android.view.View;
+
+import androidx.preference.Preference;
+
+import lineageos.preference.LineageSystemSettingListPreference;
+import lineageos.providers.LineageSettings;
+
+public class QuickPulldownPreferenceController extends BasePreferenceController {
+
+    public QuickPulldownPreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        int value = LineageSettings.System.getInt(mContext.getContentResolver(),
+                LineageSettings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 0);
+        String summary=	"";
+        switch (value) {
+            case 0:
+                summary = getResources().getString(
+                    R.string.status_bar_quick_qs_pulldown_off);
+                break;
+
+            case 1:
+            case 2:
+                summary = getResources().getString(
+                    R.string.status_bar_quick_qs_pulldown_summary,
+                    getResources().getString(
+                        (value == 2) ^
+                        (getResources().getConfiguration().getLayoutDirection()
+                            == View.LAYOUT_DIRECTION_RTL)
+                        ? R.string.status_bar_quick_qs_pulldown_summary_left
+                        : R.string.status_bar_quick_qs_pulldown_summary_right));
+                break;
+        }
+        return summary;
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+}
