d516d46684989e6fc545c30ceca776c8183ae309
Praditia Nur <pnplusplus@gmail.com>
2024-06-22 03:51:09 +0000

    Settings: Import 2-button settings from LineageParts
    Change-Id: I567e27b217b99c2cc806bdca2552e4edc31befa8

--- src/com/android/settings/gestures/TwoButtonNavigationSettingsFragment.java
+++ src/com/android/settings/gestures/TwoButtonNavigationSettingsFragment.java
@@ -26,6 +26,13 @@ import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * A fragment that includes settings for 2-button navigation modes.
  */
@@ -36,6 +43,113 @@ public class TwoButtonNavigationFragment extends DashboardFragment {
 
     public static final String TWO_BUTTON_NAVIGATION_SETTINGS =
             "com.android.settings.TWO_BUTTON_NAVIGATION_SETTINGS";
+
+    private static final String KEY_NAVIGATION_BACK_LONG_PRESS = "navigation_back_long_press";
+    private static final String KEY_NAVIGATION_HOME_LONG_PRESS = "navigation_home_long_press";
+    private static final String KEY_NAVIGATION_HOME_DOUBLE_TAP = "navigation_home_double_tap";
+
+    private ListPreference mNavigationBackLongPressAction;
+    private ListPreference mNavigationHomeLongPressAction;
+    private ListPreference mNavigationHomeDoubleTapAction;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Action defaultBackLongPressAction = Action.fromIntSafe(res.getInteger(
+                org.lineageos.platform.internal.R.integer.config_longPressOnBackBehavior));
+        Action defaultHomeLongPressAction = Action.fromIntSafe(res.getInteger(
+                org.lineageos.platform.internal.R.integer.config_longPressOnHomeBehavior));
+        Action defaultHomeDoubleTapAction = Action.fromIntSafe(res.getInteger(
+                org.lineageos.platform.internal.R.integer.config_doubleTapOnHomeBehavior));
+        Action backLongPressAction = Action.fromSettings(resolver,
+                LineageSettings.System.KEY_BACK_LONG_PRESS_ACTION,
+                defaultBackLongPressAction);
+        Action homeLongPressAction = Action.fromSettings(resolver,
+                LineageSettings.System.KEY_HOME_LONG_PRESS_ACTION,
+                defaultHomeLongPressAction);
+        Action homeDoubleTapAction = Action.fromSettings(resolver,
+                LineageSettings.System.KEY_HOME_DOUBLE_TAP_ACTION,
+                defaultHomeDoubleTapAction);
+
+        // Navigation bar back long press
+        mNavigationBackLongPressAction = initList(KEY_NAVIGATION_BACK_LONG_PRESS,
+                backLongPressAction);
+
+        // Navigation bar home long press
+        mNavigationHomeLongPressAction = initList(KEY_NAVIGATION_HOME_LONG_PRESS,
+                homeLongPressAction);
+
+        // Navigation bar home double tap
+        mNavigationHomeDoubleTapAction = initList(KEY_NAVIGATION_HOME_DOUBLE_TAP,
+                homeDoubleTapAction);
+
+        List<Integer> unsupportedValues = new ArrayList<>();
+        List<String> entries = new ArrayList<>(
+                Arrays.asList(res.getStringArray(R.array.hardware_keys_action_entries)));
+        List<String> values = new ArrayList<>(
+                Arrays.asList(res.getStringArray(R.array.hardware_keys_action_values)));
+
+        // hide split screen option unconditionally - it doesn't work at the moment
+        // once someone gets it working again: hide it only for low-ram devices
+        // (check ActivityManager.isLowRamDeviceStatic())
+        unsupportedValues.add(Action.SPLIT_SCREEN.ordinal());
+
+        for (int unsupportedValue: unsupportedValues) {
+            entries.remove(unsupportedValue);
+            values.remove(unsupportedValue);
+        }
+
+        String[] actionEntries = entries.toArray(new String[0]);
+        String[] actionValues = values.toArray(new String[0]);
+
+        mNavigationBackLongPressAction.setEntries(actionEntries);
+        mNavigationBackLongPressAction.setEntryValues(actionValues);
+
+        mNavigationHomeLongPressAction.setEntries(actionEntries);
+        mNavigationHomeLongPressAction.setEntryValues(actionValues);
+
+        mNavigationHomeDoubleTapAction.setEntries(actionEntries);
+        mNavigationHomeDoubleTapAction.setEntryValues(actionValues);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mNavigationBackLongPressAction) {
+            handleListChange((ListPreference) preference, newValue,
+                    LineageSettings.System.KEY_BACK_LONG_PRESS_ACTION);
+            return true;
+        } else if (preference == mNavigationHomeLongPressAction) {
+            handleListChange((ListPreference) preference, newValue,
+                    LineageSettings.System.KEY_HOME_LONG_PRESS_ACTION);
+            return true;
+        } else if (preference == mNavigationHomeDoubleTapAction) {
+            handleListChange((ListPreference) preference, newValue,
+                    LineageSettings.System.KEY_HOME_DOUBLE_TAP_ACTION);
+            return true;
+        }
+        return false;
+    }
+
+    private ListPreference initList(String key, Action value) {
+        return initList(key, value.ordinal());
+    }
+
+    private ListPreference initList(String key, int value) {
+        ListPreference list = getPreferenceScreen().findPreference(key);
+        if (list == null) return null;
+        list.setValue(Integer.toString(value));
+        list.setSummary(list.getEntry());
+        list.setOnPreferenceChangeListener(this);
+        return list;
+    }
+
+    private void handleListChange(ListPreference pref, Object newValue, String setting) {
+        String value = (String) newValue;
+        int index = pref.findIndexOfValue(value);
+        pref.setSummary(pref.getEntries()[index]);
+        LineageSettings.System.putInt(getContentResolver(), setting, Integer.parseInt(value));
+    }
 
     @Override
     public int getMetricsCategory() {
